e5d76cb5a4e57e8355baea466af60f189fe37d4a
刘志翔 <soraly@163.com>
2019-06-14 16:26:25 +0800

    add dangerouslySetInnerHTML

--- src/doc/todoList/demo0614.js
+++ src/doc/todoList/demo0614.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react'
-
+import Remarkable from 'remarkable'
 class TodoApp extends Component {
     constructor() {
         super()
         this.state = {
             text: '',
-            items: [
-                { id: 13, text: 'hello' },
-                { id: 23, text: 'xiang' },
-            ]
+            items: [],
+            dangerText: { __html: '<h3>hello</h3>' }
         }
     }
     handleInput(event) {
+        console.log()
         this.setState({ text: event.target.value })
     }
     handleSubmit(e) {
@@ -22,15 +21,22 @@ class TodoApp extends Component {
         }])
         this.setState({ items: nextItems, text: '' })
     }
+    
+    rawMarkup() {
+        const md = new Remarkable();
+        return { __html: this.state.text };
+    }
     render() {
         return (
             <div>
                 <h3>TODO</h3>
                 <TodoList items={this.state.items} />
                 <form onSubmit={this.handleSubmit.bind(this)} >
-                    <input onChange={this.handleInput.bind(this)} value={this.state.text} type="text" />
+                    <input ref='inp' onChange={this.handleInput.bind(this)} value={this.state.text} type="text" />
                     <button>{'Add'}</button>
                 </form>
+                <TodoTest text={this.state.text}/>
+                <div dangerouslySetInnerHTML={this.rawMarkup()}></div>
             </div>
         )
     }
@@ -45,5 +51,29 @@ const TodoList = props => (
         }
     </ul>
 )
+class TodoTest extends Component {
+    componentWillMount() {
+        console.log('componentWillMount');
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    componentWillReceiveProps() {
+        console.log('componentWillReceiveProps');
+    }
+    componentWillUpdate() {
+        console.log('componentWillUpdate');
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+    }
+    render(){
+        
+        return <div>test,{this.props.text}</div>
+    }
+}
 
 export default TodoApp
